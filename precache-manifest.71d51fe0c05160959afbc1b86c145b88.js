c565a4817dbbcf42249f66b2277485ddf8a5f8b4
Freek van Polen <tubyhes@gmail.com>
2019-06-11 20:50:51 +0200

    Updates

---
+++ precache-manifest.71d51fe0c05160959afbc1b86c145b88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf3af965f6db1a0de96b4828728e408",
+    "url": "/regelaar-wc-calculator/index.html"
+  },
+  {
+    "revision": "4b44ce581872298dc765",
+    "url": "/regelaar-wc-calculator/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7ccd9edca80113bfb509",
+    "url": "/regelaar-wc-calculator/static/js/2.735eb377.chunk.js"
+  },
+  {
+    "revision": "4b44ce581872298dc765",
+    "url": "/regelaar-wc-calculator/static/js/main.eb35776b.chunk.js"
+  },
+  {
+    "revision": "3e0edb7819a80de2d16d",
+    "url": "/regelaar-wc-calculator/static/js/runtime~main.aed79a59.js"
+  }
+]);
