d59c914ff2e00a605e25d3bb95f30f1b01ebb403
De-Regelaar <heko@regelaar.nl>
2020-09-25 11:48:29 +0200

    Updates

---
+++ precache-manifest.e733819fb2e8eba525f36efd8b3ab9db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bfeb2163a06fb59272d7787ef98d21c",
+    "url": "/ERC/index.html"
+  },
+  {
+    "revision": "74510e1023e10f0aa0c6",
+    "url": "/ERC/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "dafdb7d5738911a04764",
+    "url": "/ERC/static/js/2.be8e36d6.chunk.js"
+  },
+  {
+    "revision": "74510e1023e10f0aa0c6",
+    "url": "/ERC/static/js/main.f9c6f8ad.chunk.js"
+  },
+  {
+    "revision": "dcd9307119988b3d4439",
+    "url": "/ERC/static/js/runtime~main.89799ce7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/De-Regelaar/ERC/precache-manifest.682c9b1151dc717973cf1be9b081985e.js"
+  "/ERC/precache-manifest.e733819fb2e8eba525f36efd8b3ab9db.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/De-Regelaar/ERC/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ERC/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
