4d68a1e7452a7f9abe72dbf3a5163f4ebf222ea8
De-Regelaar <heko@regelaar.nl>
2020-09-29 13:03:49 +0200

    changelog

--- src/Calculator.js
+++ src/Calculator.js
@@ -7,6 +7,10 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import FormGroup from '@material-ui/core/FormGroup';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormLabel from '@material-ui/core/FormLabel';
 
 import Result from "./Result.js";
 
@@ -17,15 +21,15 @@ class Calculator extends React.Component
 		super(props);
 
 		this.state = {
-			bezoekers: 100,
-			gezelschap: 10,
-			binnen: false,
-			doorstroom: false,
-			terras: false,
+			
+			
+			
+			
 		};
 
 	
 		this.handleBezoekersChange = this.handleBezoekersChange.bind(this);
+		this.handleInruimteChange = this.handleInruimteChange.bind(this);
 		this.handleGezelschapChange = this.handleGezelschapChange.bind(this);
 		this.handleVeiligheidsregioChange = this.handleVeiligheidsregioChange.bind(this);
 		this.handleBinnenChange = this.handleBinnenChange.bind(this);
@@ -35,6 +39,13 @@ class Calculator extends React.Component
 
 	
 	handleBezoekersChange (event) 
+	{
+		if (!isNaN(parseInt(event.target.value))) 
+		{
+			this.setState({[event.target.name]: parseInt(event.target.value)});
+		}
+	}
+	handleInruimteChange (event) 
 	{
 		if (!isNaN(parseInt(event.target.value))) 
 		{
@@ -55,12 +66,12 @@ class Calculator extends React.Component
 
 	handleBinnenChange (event) 
 	{
-		this.setState({"binnen": event.target.checked});
+		this.setState({"binnen": event.target.value});
 	}
 
 	handleDoorstroomChange (event) 
 	{
-		this.setState({"doorstroom": event.target.checked});
+		this.setState({"doorstroom": event.target.value});
 	}
 
 	handleTerrasChange (event) 
@@ -72,33 +83,43 @@ class Calculator extends React.Component
 	{
 		
 				
-		let verboden = {		
+		const verboden = {		
 			name: "Verboden?",
-			uitkomst: (this.state.gezelschap >=50) && (this.state.veiligheidsregio>=199 && this.state.veiligheidsregio<=299)?<Typography color='Secondary'>ja</Typography>:"nee",
-			grens: "In regio's die in risiconiveau 2 vallen kunnen geen evenementen plaatsvinden met grote gezelschappen. Ook niet na een melding.",
+			uitkomst: (this.state.doorstroom >= "3") || (this.state.gezelschap > 4) || ((this.state.inruimte >30) && (this.state.binnen === "binnen")) || ((this.state.bezoekers >40) && (this.state.binnen === "buiten") && (this.state.doorstroom >1))?<Typography color='Secondary'>ja</Typography>:"nee",
+			grens: "Let op: voor sommige culturele gebouwen kan een ontheffing worden aangevraagd waardoor er wel meer dan 30 personen binnen mogen (per ruimte).",
 		};	
 
-		const melding = {
-			name: "Melding doen",
-			uitkomst:(this.state.bezoekers >=50) && (this.state.veiligheidsregio>=199 && this.state.veiligheidsregio<=299)?<Typography color='Secondary'>ja</Typography>:"nee",
-			grens: "50 of meer bezoekers in regio's in risiconiveau 2.",
-		};
+		const ontheffing = {		
+			name: "Ontheffing",
+			uitkomst: ((this.state.inruimte >30) && (this.state.binnen === "binnen")) ?<Typography color='Secondary'>ja</Typography>:"nee",
+			grens: "Voor meer dan 30 personen per ruimte (binnen) dient een ontheffing aangevraagd te worden. Dit kan voor 'panden die van groot belang zijn, bijvoorbeeld culturele instellingen'.",
+		};	
+
+		const maatwerk = {		
+			name: "Maatwerk",
+			uitkomst: (this.state.doorstroom <=1)?<Typography color='Secondary'>ja</Typography>:"nee",
+			grens: "Voor doorstroom evenementen (binnen en buiten) dienen afspraken gemaakt te worden over het aantal personen per vierkante meter.",
+		};	
 
 		const placeren = {
 			name: "Bezoeker placeren",
-			uitkomst: ((this.state.bezoekers>=250) && (this.state.binnen<1) && (this.state.doorstroom<1)) 
-			|| ((this.state.binnen>=1) && (this.state.doorstroom<1))
-			|| (this.state.terras>=1)?<Typography color='Secondary'>ja</Typography>:"nee",
-			grens: "Binnen moet bij alle aantallen worden geplaceerd. Buiten is dat verplicht vanaf 250 bezoekers. Voor beide is er een uitzondering in het geval van continue doorstroom.",
+			uitkomst: (this.state.doorstroom >1)?<Typography color='Secondary'>ja</Typography>:"nee",
+			grens: "Met uizondering van evenementen met een continue doorstroom dienen alle bezoekers geplaceerd te zijn.",
 		};
 
 		const check = {
 			name: "Gezondheidscheck",
-			uitkomst: ((this.state.bezoekers>=250) && (this.state.binnen<1) && (this.state.doorstroom<1))
-			 || ((this.state.bezoekers>=100) && (this.state.binnen>=1) && (this.state.doorstroom<1))
-			 || (this.state.terras>=1) ? <Typography color='Secondary'>ja</Typography>:"nee",
-			grens: "Binnen vanaf 100 bezoekers, buiten bij vanaf 250. Bij doorstroom evenement nooit.",
+			uitkomst: ((this.state.inruimte>=100) && (this.state.binnen === "binnen") && (this.state.doorstroom >1)) ? <Typography color='Secondary'>ja</Typography>:"nee",
+			grens: "Binnen vanaf 100 bezoekers. Door de verlaging van het aantal mensen buiten hoeft er buiten, of bij doorstroom evenementen geen gezondheidscheck te worden gedaan." ,
 		};
+
+		const reservering = {
+			name: "Reservering",
+			uitkomst: (this.state.inruimte >100) && (this.state.binnen === "binnen") || (this.state.doorstroom <=1) ? <Typography color='Secondary'>ja</Typography>:"nee",
+			grens: "Binnen vanaf 100 bezoekers. Bij doorstroom evenementen moeten afspraken gemaakt worden over aantallen en tijdslots.",
+		};
+
+
 		return (
 			<React.Fragment>
 				<Grid container spacing={2} 
@@ -108,21 +129,54 @@ class Calculator extends React.Component
 						
 					</Grid>
 					<Grid item xs={12}>
-						<Typography color='primary'>Versie 28 september 2020 </Typography>
+						<Typography color='primary'>Versie 2.0 gepubliceerd op 29 september 2020 </Typography>
 						<Typography variant="body2" color='secondary'> Dit formulier is een hulpmiddel, er kunnen geen rechten ontleend worden aan de uitslag. </Typography>
 						<Typography variant="body2" color='secondary'> In verschillende regio's kunnen aanvullende maatregelen gelden die niet in dit formulier zijn opgenomen. </Typography>
 						<Typography variant="body1" color='TextSecondary'> Vul het formulier volledig in voor het beste resultaat. </Typography>
 					</Grid>
+					
+
+					<Grid item xs={12}>
+					<Typography variant="h6">Locatie en bezoekers aantallen</Typography>
+					</Grid>
+
+      	<Grid item xs={12}
+	      	align="left">
+						<FormControl component="fieldset">
+      <FormLabel component="legend">Waar vindt het evenement plaats</FormLabel>
+      <RadioGroup aria-label="Binnen" name="binnen" value={this.state.binnen} onChange={this.handleBinnenChange}>
+        <FormControlLabel value="binnen" control={<Radio />} label="Binnen" />
+        <FormControlLabel value="buiten" control={<Radio />} label="Buiten" />
+      </RadioGroup>
+    </FormControl>
+	      	</Grid>
+			
+			
 					<Grid item xs={12}
 					align="left">
 						<TextField 
 							id="bezoekers"
 							name="bezoekers"
-							label='Totaal aantal bezoekers' 
+							label='Totaal aantal bezoekers buitenevenement (incl kinderen, excl personeel)' 
 							type="number"
 							fullWidth
 							value = {this.state.bezoekers} 
 							onChange={this.handleBezoekersChange}
+							disabled = {!this.state.binnen || this.state.binnen === "binnen"}
+						/>
+					</Grid>
+
+			<Grid item xs={12}
+					align="left">
+						<TextField 
+							id="inruimte"
+							name="inruimte"
+							label='Maximaal aantal bezoekers in een binnenruimte (incl kinderen, excl personeel)' 
+							type="number"
+							fullWidth
+							value = {this.state.inruimte} 
+							onChange={this.handleInruimteChange}
+							disabled = {!this.state.binnen || this.state.binnen === "buiten"}
 						/>
 					</Grid>
 				
@@ -136,102 +190,31 @@ class Calculator extends React.Component
 							fullWidth
 							value = {this.state.gezelschap} 
 							onChange={this.handleGezelschapChange}
+							disabled = {!this.state.binnen}
 						/>
 					</Grid>
 					
-					<Grid item xs={12}
-					align="left">
-												
-						 <FormControl required>
-        <InputLabel id="VR">Veiligheidsregio</InputLabel>
-        <Select
-          labelId="veiligheidsregio"
-          id="veiligheidsregio"
-          value={this.state.veiligheidsregio}
-          onChange={this.handleVeiligheidsregioChange}
-                  >
-          <MenuItem value="">
-            <em></em>
-          </MenuItem>
-<MenuItem value={212 }>	    Amsterdam-Amstelland		</MenuItem>
-<MenuItem value={200 }>	    Brabant-Noord		</MenuItem>
-<MenuItem value={201 }>	    Brabant-Zuidoost		</MenuItem>
-<MenuItem value={10 }>	    Drenthe		</MenuItem>
-<MenuItem value={202 }>	    Flevoland		</MenuItem>
-<MenuItem value={11 }>	    Friesland		</MenuItem>
-<MenuItem value={12 }>	    Gelderland-Midden		</MenuItem>
-<MenuItem value={203 }>	    Gelderland-Zuid		</MenuItem>
-<MenuItem value={204 }>	    Gooi en Vechtstreek		</MenuItem>
-<MenuItem value={213 }>	    Groningen		</MenuItem>
-<MenuItem value={205 }>	    Haaglanden		</MenuItem>
-<MenuItem value={206 }>	    Hollands Midden		</MenuItem>
-<MenuItem value={14 }>	    IJsselland		</MenuItem>
-<MenuItem value={207 }>	    Kennemerland		</MenuItem>
-<MenuItem value={15 }>	    Limburg-Noord		</MenuItem>
-<MenuItem value={16 }>	    Midden- en West-Brabant		</MenuItem>
-<MenuItem value={17 }>	    Noord- en Oost-Gelderland		</MenuItem>
-<MenuItem value={18 }>	    Noord-Holland Noord		</MenuItem>
-<MenuItem value={208 }>	    Rotterdam-Rijnmond		</MenuItem>
-<MenuItem value={20 }>	    Twente		</MenuItem>
-<MenuItem value={209 }>	    Utrecht		</MenuItem>
-<MenuItem value={210 }>	    Zaanstreek-Waterland		</MenuItem>
-<MenuItem value={23 }>	    Zeeland		</MenuItem>
-<MenuItem value={211 }>	    Zuid-Holland Zuid		</MenuItem>
-<MenuItem value={24 }>	    Zuid-Limburg		</MenuItem>
-        </Select>
-        <FormHelperText>In welke Veiligheidsregio vindt het plaats?</FormHelperText>
-      </FormControl>
-
-		
-					</Grid>
-
-
+					
 					<Grid item xs={12}>
 					<Typography variant="h6">Is er op het evenement sprake van een hieronder staande situatie? </Typography>
 					</Grid>
+					
+
 					<Grid item xs={12}
 					align="left">
-						<FormControlLabel
-		        	control={
-			          <Checkbox
-			            checked={this.state.doorstroom}
-			            onChange={this.handleDoorstroomChange}
-			            value="checkedB"
-			            color="primary"
-			          />
-			        }
-			        label=<Typography variant="body2">Is er sprake van een evenement met continue doorstroom van publiek?</Typography>
-		      	/>
-	      	</Grid>
+						<FormControl component="fieldset">
+      <FormLabel component="legend">Type evenement</FormLabel>
+      <RadioGroup aria-label="Doorstroom" name="doorstroom" value={this.state.doorstroom} onChange={this.handleDoorstroomChange}>
+        <FormControlLabel value="1" control={<Radio />} label="Continue doorstroom van bezoekers" />
+        <FormControlLabel value="2" control={<Radio />} label="Alle bezoekers zitten" />
+        <FormControlLabel value="3" control={<Radio />} label="Bezoekers zitten maar er is sprake van roulatie" />
+        
+      </RadioGroup>
+    </FormControl>
+ <Divider variant="middle"/>
+		      	</Grid>
 
-	      	<Grid item xs={12}
-	      	align="left">
-						<FormControlLabel
-		        	control={
-			          <Checkbox
-			            checked={this.state.binnen}
-			            onChange={this.handleBinnenChange}
-			            value="checkedB"
-			            color="primary"
-			          />
-			        }
-			        label=<Typography variant="body2">Vindt het evenement binnen plaats?</Typography>
-		      	/>
-	      	</Grid>
-			<Grid item xs={12}
-				align="left">
-						<FormControlLabel
-		        	control={
-			          <Checkbox
-			            checked={this.state.terras}
-			            onChange={this.handleTerrasChange}
-			            value="checkedB"
-			            color="primary"
-			          />
-			        }
-			        label=<Typography variant="body2">Is er sprake van een buitenterras (eet- of drink gelegenheid)?</Typography>
-		      	/>
-	      	</Grid>
+	
 
 				</Grid>
 	      <Divider variant="middle"/>	
@@ -242,7 +225,7 @@ class Calculator extends React.Component
 	      		
 	      	</Grid>
 	      	<Grid item xs={12}>
-	      		<Result rows={[verboden, melding, placeren, check]} />
+	      		<Result rows={[verboden, ontheffing, maatwerk, placeren, check, reservering]} />
 	      	</Grid>
 	      	<Grid item xs={12}>
 	      		<Typography variant="subtitle" color="TextSecondary">Voor meer informatie over de definities en lokale regels wordt u verwezen naar de website van de relevante veiligheidsregio.</Typography>
